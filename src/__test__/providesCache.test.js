eae238cb740ce31f4080b76bfb47231194d992ed
Frankie Bagnardi <f.bagnardi@gmail.com>
2015-11-04 17:29:17 -0700

    testing frameworking

--- src/__test__/providesCache.test.js
+++ src/__test__/providesCache.test.js
@@ -1,10 +1,35 @@
+import 'babel-polyfill';
 import {expect} from 'chai';
 import jsdom from 'mocha-jsdom';
 import providesCache from '../providesCache';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ObCache from 'obcache';
 
 describe('providesCache', () => {
   jsdom();
+  var render, inst, node, cache, obs;
+  beforeEach(() => {
+    var el = document.createElement('div');
+    document.body.appendChild(el);
+    cache = new ObCache();
+    cache.register('id', async ([x]) => x);
+    obs = cache.get('id', 'a');
+    obs.set('b');
 
-  it(
+    render = (reactElement) => {
+      inst = ReactDOM.render(reactElement, el);
+      node = ReactDOM.findDOMNode(inst);
+      return inst;
+    }
+  });
+
+  it('is a sane testing environment', () => {
+    render(<div>x</div>);
+    expect(node.textContent).to.equal('x');
+    console.error(inst.children);
+    expect(inst.props.children).to.equal('x');
+  })
+
 });
 
